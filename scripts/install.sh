75ffb730110316a973f6d5b526e3d0678f394c81
Aurelien CARLE <aurelien.carle@gmail.com>
2021-02-24 00:18:56 +0100

    install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+echo "##########################"
+echo "## PyPlotFramework v2.0 ##"
+echo "##########################"
+
+sleep 1
+echo " > Results directory creation"
+mkdir -p results/{2016,2017,2018}/{comparaison,flattree}
+
+sleep 1
+echo " > Inputs directory creation"
+mkdir -p inputs/{2016,2017,2018}/{DATA,MC}
+mkdir -p inputs/{pheno,timed}
+
+sleep 1
+echo " > Combine directory creation"
+mkdir -p combine/{2016,2017,2018}/{inclusive,interference,one_bin,unrolled}/{inputs,results}
+
+sleep 1
+echo "Installtion finished ! :)"
