b6cca960ac5097d7ef07842c8c49e9beb0266bd6
sandhya jain <sandhya.jain@cern.ch>
2022-06-28 13:55:38 +0200

    fixing to read root files correctly

--- bin/data_mc_comparaison_splitted.py
+++ bin/data_mc_comparaison_splitted.py
@@ -24,11 +24,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('observable', help='display your observable')
 parser.add_argument('year', help='year of samples')
 parser.add_argument('title', help='display your observable title')
+parser.add_argument('timebin', help='display the time bin')
 
 args = parser.parse_args()
 observable = args.observable
 year = args.year
 title = args.title
+timebin = int(args.timebin)
 
 def integral_complete(histo, max_bin): 
      return histo.Integral()
@@ -50,7 +52,16 @@ canvas = TCanvas('stack_'+observable,'stack_'+observable, 800, 800)
 canvas.UseCurrentStyle()
 
 datafile_input = TFile('./results/'+year+'/flattree/'+observable+'_data.root')
-rootfile_input = TFile('./results/'+year+'/flattree/'+observable+'_forComp.root')
+stimebin="";
+if (timebin==-1):
+     stimebin = "_puold";
+if (timebin==-2):
+     stimebin = "_punew";
+if (timebin==-3):
+     stimebin = "_puinc";
+if (timebin>=0):
+     stimebin = "_put"+str(timebin);
+rootfile_input = TFile('./results/'+year+'/flattree/'+observable+'_forComp'+stimebin+'.root')
 
 ################################################################################
 ## Create Histo 
