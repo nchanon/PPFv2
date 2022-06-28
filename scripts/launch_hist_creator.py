b1d213c612a1f75b8d1be8be026aeda724262456
sandhya jain <sandhya.jain@cern.ch>
2022-06-28 13:59:03 +0200

    to run with weight options

--- scripts/launch_hist_creator.py
+++ scripts/launch_hist_creator.py
@@ -7,31 +7,31 @@ import argparse
 
 observables = [
     'm_dilep',
-    'pt_elec',
-    'eta_elec',
-    'pt_muon',
-    'eta_muon',
-    'n_jets',
-    'n_bjets',
-#    'pt_lead',
-#    'pt_sublead',
-    'b1_pt',
-    'j1_pt',
-    'j1_eta',
-    'b1_eta'
-#    'b2_pt',
-    'j2_pt',
-    'j2_eta',
-#    'b2_eta',
-    'pt_emu'
-#    'dijet_m'
+#    'pt_elec',
+#    'eta_elec',
+#    'pt_muon',
+#    'eta_muon',
+#    'n_jets',
+#    'n_bjets',
+##    'pt_lead',
+##    'pt_sublead',
+#    'b1_pt',
+#    'j1_pt',
+#    'j1_eta',
+#    'b1_eta'
+##    'b2_pt',
+#    'j2_pt',
+#    'j2_eta',
+##    'b2_eta',
+#    'pt_emu'
+##    'dijet_m'
 ]
 
 years = [
-    '2016',
+ #   '2016',
     '2017'
 ]
 
 for y in years:
     for o in observables:
-        os.system('./bin/histograms_creator '+o+' '+y+' forComp')
+        os.system('./bin/histograms_creator '+o+' '+y+' forComp'+ ' -1' )
